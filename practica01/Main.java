4c87cdbb295b819e1f9a1ad5c4979e97290a6f34
esau2001 <esmap@ciencias.unam.mx>
2019-08-18 21:32:05 -0500

    Practica 2

---
+++ practica01/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+public static void main(String[]args){
+System.out.println("hola");
+System.out.println("openjdk 11.0.4 2019-07-16");
+System.out.println("OpenJDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3");
+System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3, mixed mode, sharing)");
+}
+}
+
