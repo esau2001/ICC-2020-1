329faae750f1f0427d737397ca092a4a58aa9aa9
esau2001 <esmap@ciencias.unam.mx>
2019-09-04 21:17:02 -0500

    CANCIÓN-4/08/19

---
+++ practica05/HeyJude.java
@@ -0,0 +1,91 @@
+public class HeyJude {
+
+public static void main(String[] args) {
+
+String hey = "hey Jude";
+
+String dt = "don't";
+String make = "make it bad";
+String take = "take a sad a song and make it better";
+String be = "be afraid";
+String were = "you were made to go out and get her";
+String leet = "let me down";
+String you = "you have found her, now go and get her";
+
+String remember = "remember to";
+String let = "let her into you heart";
+String her = "let her under your skin";
+
+String then = "then you";
+String can = "can start";
+String beg = "begin";
+String to = "to make it better";
+
+String bet = "better";
+String wa = "waaa";
+String na = "na";
+
+int p = 0; 
+while (p<7) {
+	if (p==0 || p==1 || p==3 || p==5){
+		System.out.print(hey);
+                System.out.print(dt);
+    		System.out.print(remember);  
+		System.out.println(then);   
+		System.out.print(to);   
+p++;    
+	}
+
+        if (p==0 || p==5){
+		System.out.print(make);
+		System.out.println(take);
+	}
+	
+
+	if (p==0 || p==3){
+		System.out.print(let);
+		System.out.print(can);
+
+	}
+	
+	
+	if (p==1){
+		System.out.print(be);
+			System.out.println(were);
+	}
+
+	
+
+	if (p==1 || p==5){
+		System.out.print(her);
+		System.out.print(beg);
+	}
+
+	if (p==3){
+		System.out.print(leet);
+		System.out.println(you);
+	}
+
+	
+}
+
+int n = 0; 
+
+	if (p==2 || p==4 || p==6){while (n<10)
+		System.out.print(na);
+
+p++;
+}
+ 	if (p==5){while (n<10)
+		System.out.print(bet); 
+		System.out.print(wa); 
+}
+
+	if (p==6){while (n<10)
+		System.out.print(hey);
+}
+
+
+
+}
+}
